f8b520a6d3435090d6f6286edf6aaa80b5c920ff
Bruno Vinicius Lópes <bruno.lopes@catolicasc.edu.br>
2023-02-24 18:20:41 -0300

    Primeiro commit, arquivo bruto de configurações

---
+++ src/main/java/com/top5nacional/trucomanosbackend/TrucomanosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.top5nacional.trucomanosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucomanosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucomanosBackendApplication.class, args);
+	}
+
+}
